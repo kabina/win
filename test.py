23765afb95345bfd58c5010e0a1074130a1449f1
kabina <nheo.an@gmail.com>
2021-07-08 16:38:46 +0900

    filter and max

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-for i in range(10):
-    print(i)
+l = [1,2,3,4,2,3,45,23,2,3,4]
+c = max(set(l), key=l.count)
+print(c)
+
+f = filter(lambda x : x > 2, l)
+print(list(f))
 
 s = "".join([f"{i}" if i > 0 else "NULL" for i in range(10)])
-print(s)
+#print(s)
