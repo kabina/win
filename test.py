29a75a9bf343321aa74fa1ab1b9a11eae8195c08
ChangSun An <nheo.an@gmail.com>
2021-08-30 16:17:07 +0900

    최초업로드

--- test.py
+++
@@ -1,9 +0,0 @@
-l = [1,2,3,4,2,3,45,23,2,3,4]
-c = max(set(l), key=l.count)
-print(c)
-
-f = filter(lambda x : x > 2, l)
-print(list(f))
-
-s = "".join([f"{i}" if i > 0 else "NULL" for i in range(10)])
-print(s)
